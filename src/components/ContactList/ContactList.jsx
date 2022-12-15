bd2905b476a9af2dc29433d822a698859fd7b396
Mylymuk Oksana <oksana.mylymukk@gmail.com>
2022-12-16 01:30:38 +0200

    finished

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,17 +4,21 @@ import { ContactListItem } from '../ContactListItem/ContactListItem';
 import css from './ContactList.module.css';
 
 export const ContactList = ({ contacts, onClick }) => {
-  return (
-    <ul className={css.contactsList}>
-      {contacts.map(contact => (
-        <ContactListItem
-          key={contact.id}
-          contact={contact}
-          onClick={() => onClick(contact.id)}
-        />
-      ))}
-    </ul>
-  );
+  if (contacts.length > 0) {
+    return (
+      <ul className={css.contactsList}>
+        {contacts.map(contact => (
+          <ContactListItem
+            key={contact.id}
+            contact={contact}
+            onClick={() => onClick(contact.id)}
+          />
+        ))}
+      </ul>
+    );
+  } else {
+    return <p className={css.noContactsText}>No contacts yet</p>;
+  }
 };
 
 ContactList.propTypes = {
